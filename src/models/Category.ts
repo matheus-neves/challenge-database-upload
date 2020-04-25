43829cb84790d0a7d5ce6efcf4fe3b3ce4812ef5
Matheus Neves <matheusneves1989@gmail.com>
2020-04-25 10:29:12 -0300

    feat: add eager relations transaction/category

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -3,8 +3,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   Column,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -13,6 +15,9 @@ class Category {
 
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @CreateDateColumn()
   created_at: Date;
